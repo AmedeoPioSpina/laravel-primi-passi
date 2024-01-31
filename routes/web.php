9c55bc525e5c85a4f24787d7154100529097a469
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-01-31 01:38:09 +0100

    finito

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $testo = 'Questi sono dati passati tramite la Blade';
+    return view('home', ['testo' => $testo]);
 });
